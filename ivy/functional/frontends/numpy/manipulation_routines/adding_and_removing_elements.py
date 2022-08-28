9aea32c10f1ec94e4c4e9a2579e7902ff8c86e91
Jackie Chen <cheezballz3.14@gmail.com>
2022-08-28 14:44:39 -0400

    Add insert stub

--- ivy/functional/frontends/numpy/manipulation_routines/adding_and_removing_elements.py
+++ ivy/functional/frontends/numpy/manipulation_routines/adding_and_removing_elements.py
@@ -13,3 +13,7 @@ def delete(arr, obj, axis=None):
     obj.asarray(out=obj)
     includeList = [x for x in range(arr.shape[axis]) if x not in obj]
     return [ivy.gather(arr, i, axis=axis) for i in includeList]
+
+
+def insert(arr, obj, values, *, axis):
+    return
